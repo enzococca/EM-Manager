b36d97fc8fe684c16d32903e1552b1e8aa54e53a
Emanuel Demetrescu <emanuel.demetrescu@gmail.com>
2023-05-19 14:00:57 +0200

    modifiche minori e winreg fixed

--- Ed2graphml.bat
+++ Ed2graphml.bat
@@ -1,24 +1,5 @@
 @echo off
 
-REM Controlla se Python è installato
-where python >nul 2>&1
-if %errorlevel% neq 0 (
-    REM Python non è installato, quindi lo installa utilizzando PackageManagement
-    echo Python non trovato, lo sto installando...
-    powershell -command "Install-Package python"
-)
-
-REM Controlla se PyQt5 è installato
-python -c "import PyQt5" >nul 2>&1
-if %errorlevel% neq 0 (
-    REM PyQt5 non è installato, quindi lo installa utilizzando PackageManagement
-    echo PyQt5 non trovato, lo sto installando...
-    powershell -command "Install-Package pyqt5"
-)
-
-REM Avvia il programma principale con lo splash screen
-rem start "" /B python splash.py
-
 
 REM Crea un ambiente virtuale in Python se non esiste già
 if not exist ENV (
@@ -31,6 +12,7 @@ call ENV\Scripts\activate.bat
 REM Installa le dipendenze necessarie usando pip all'interno dell'ambiente virtuale e registra l'output nel log dello splash screen
 python -m pip install --upgrade pip > temp.log 2>&1
 python -m pip install -r requirements.txt >> temp.log 2>&1
+start "" /B python EDMatrix2Graphml.py
 
 REM Disattiva l'ambiente virtuale
 deactivate
